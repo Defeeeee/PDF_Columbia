33a32761f8ca65f650d00dd3c20143f3502b079f
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2025-02-28 21:59:34 -0300

    fix: Update file name template placeholder from {cuil} to {dni} in PDF processor
    Signed-off-by: Federico Diaz Nemeth <fdiaznemeth@gmail.com>

--- streamlit.py
+++ streamlit.py
@@ -6,7 +6,7 @@ from script import extract_cuil, split_pdf, zip_pdfs
 st.title('Procesador de archivos PDF')
 
 uploaded_files = st.file_uploader("Subir Archivos PDF", type="pdf", accept_multiple_files=True)
-file_name_template = st.text_input("Ingrese el nombre deseado de cada PDF.", "{cuil}_Recibos_Vacaciones_2025-02.pdf")
+file_name_template = st.text_input("Ingrese el nombre deseado de cada PDF.", "{dni}_Recibos_Vacaciones_2025-02.pdf")
 zip_name = st.text_input("Ingrese el nombre deseado para el zip", "vacaciones.zip")
 
 if st.button("Procesar"):
@@ -16,7 +16,7 @@ if st.button("Procesar"):
 
         for uploaded_file in uploaded_files:
             pdf_bytes = BytesIO(uploaded_file.read())
-            generated_files, last_cuil = split_pdf(pdf_bytes, last_cuil, file_name_template)
+            generated_files, last_cuil = split_pdf(pdf_bytes, last_cuil, file_name_template.replace("{dni}", "{cuil}"))
             all_generated_files.extend(generated_files)
 
         zip_buffer = BytesIO()
