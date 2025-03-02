05afd99518a6afbdd540f97ad4b5a6c54d9e56ac
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2025-03-02 15:06:53 -0300

    feat: Add function to zip generated PDF files
    Signed-off-by: Federico Diaz Nemeth <fdiaznemeth@gmail.com>

--- script.py
+++ script.py
@@ -24,3 +24,11 @@ def split_pdf(input_pdf, last_cuil=None, file_name_template="{cuil}_Recibos_Grat
         else:
             print(f'CUIL not found on page {i + 1}')
     return generated_files, last_cuil
+
+def zip_pdfs(pdf_files, output_zip):
+    with zipfile.ZipFile(output_zip, 'w') as zipf:
+        for pdf_name, pdf_writer in pdf_files:
+            pdf_bytes = BytesIO()
+            pdf_writer.write(pdf_bytes)
+            pdf_bytes.seek(0)
+            zipf.writestr(pdf_name, pdf_bytes.read())
