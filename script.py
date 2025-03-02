4df53d42b66dc4047309577a52b8cf5ca1987110
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2025-03-02 15:18:34 -0300

    feat: Add input for excluding specific CUIL values in PDF processing
    Signed-off-by: Federico Diaz Nemeth <fdiaznemeth@gmail.com>

--- script.py
+++ script.py
@@ -4,14 +4,14 @@ import zipfile
 import os
 from io import BytesIO
 
-def extract_cuil(text, last_cuil, regex_pattern):
+def extract_cuil(text, last_cuil, regex_pattern, exclude_match):
     matches = re.findall(regex_pattern, text)
     for match in matches:
-        if match != '51763749' and match != last_cuil:
+        if match != exclude_match and match != last_cuil:
             return match
     return None
 
-def split_pdf(input_pdf, last_cuil=None, file_name_template="{cuil}_Recibos_Gratificacion_2025-02.pdf", regex_pattern=r'\b\d{2}-(\d{8})-\d\b'):
+def split_pdf(input_pdf, last_cuil=None, file_name_template="{cuil}_Recibos_Gratificacion_2025-02.pdf", regex_pattern=r'\b\d{2}-(\d{8})-\d\b', exclude_match=""):
     generated_files = []
     reader = PyPDF2.PdfReader(input_pdf)
     num_pages = len(reader.pages)
@@ -19,7 +19,7 @@ def split_pdf(input_pdf, last_cuil=None, file_name_template="{cuil}_Recibos_Grat
     for i in range(num_pages):
         page = reader.pages[i]
         text = page.extract_text()
-        cuil = extract_cuil(text, last_cuil, regex_pattern)
+        cuil = extract_cuil(text, last_cuil, regex_pattern, exclude_match)
 
         if cuil:
             last_cuil = cuil
@@ -30,7 +30,6 @@ def split_pdf(input_pdf, last_cuil=None, file_name_template="{cuil}_Recibos_Grat
         else:
             print(f'CUIL not found on page {i + 1}')
     return generated_files, last_cuil
-
 def zip_pdfs(pdf_files, output_zip):
     with zipfile.ZipFile(output_zip, 'w') as zipf:
         for pdf_name, pdf_writer in pdf_files:
